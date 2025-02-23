0ab9f6a7b3993c85f753ba7d05a9a8ace977343b
prrao87 <prrao87@gmail.com>
2025-02-23 11:13:04 -0500

    Update site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://prrao87.github.io',
   // base: '/docs',
   trailingSlash: 'never',
 
